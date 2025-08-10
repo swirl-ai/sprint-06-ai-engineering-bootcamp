71b2e69d76dd54c0dae0ec63e8beaca558e13556
aurimasgriciunas <griciunas.aurimas@gmail.com>
2025-08-11 00:52:18 +0300

    update quality gate threshold

--- evals/eval_coordinator_agent.py
+++ evals/eval_coordinator_agent.py
@@ -6,7 +6,7 @@ from src.api.rag.graph import State
 from src.api.core.config import config
 
 
-ACC_THRESHOLD = 0.8
+ACC_THRESHOLD = 0.9
 SLEEP_TIME = 5
 
 ls_client = Client(api_key=config.LANGSMITH_API_KEY)
