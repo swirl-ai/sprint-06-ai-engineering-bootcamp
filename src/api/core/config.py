fb6e07dbd75b410c6db1430aebbff9a73afc8066
aurimasgriciunas <griciunas.aurimas@gmail.com>
2025-08-11 00:46:07 +0300

    Fix config

--- src/api/core/config.py
+++ src/api/core/config.py
@@ -3,18 +3,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Config(BaseSettings):
     OPENAI_API_KEY: str
     GROQ_API_KEY: str
-    GOOGLE_API_KEY: str
-    QDRANT_URL: str
+    GOOGLE_API_KEY: str = ""
+    QDRANT_URL: str = ""
     QDRANT_COLLECTION_NAME_ITEMS: str = "Amazon-items-collection-02-items"
     QDRANT_COLLECTION_NAME_REVIEWS: str = "Amazon-items-collection-02-reviews"
-    EMBEDDING_MODEL: str
-    EMBEDDING_MODEL_PROVIDER: str
-    GENERATION_MODEL: str
-    GENERATION_MODEL_PROVIDER: str
-    LANGSMITH_TRACING: bool
-    LANGSMITH_ENDPOINT: str
+    EMBEDDING_MODEL: str = ""
+    EMBEDDING_MODEL_PROVIDER: str = ""
+    GENERATION_MODEL: str = ""
+    GENERATION_MODEL_PROVIDER: str = ""
+    LANGSMITH_TRACING: bool = False
+    LANGSMITH_ENDPOINT: str = "https://api.langsmith.com"
     LANGSMITH_API_KEY: str
-    LANGSMITH_PROJECT: str
+    LANGSMITH_PROJECT: str = ""
     COORDINATOR_AGENT_PROMPT_TEMPLATE_PATH: str = "src/api/rag/prompts/coordinator_agent.yaml"
     PRODUCT_QA_AGENT_PROMPT_TEMPLATE_PATH: str = "src/api/rag/prompts/product_qa_agent.yaml"
     SHOPPING_CART_AGENT_PROMPT_TEMPLATE_PATH: str = "src/api/rag/prompts/shopping_cart_agent.yaml"
